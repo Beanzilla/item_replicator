38d80dad474add5380d1be43df4a9db555121d39
david <beanzilla@red-green.com>
2021-09-07 18:14:23 -0400

    Prevented Area Containers from replication
      Due to the fact there can only be a limited number of containers to
    begin with, let's not allow containers to be replicated/mass produced.

--- mods/area_containers.lua
+++ mods/area_containers.lua
@@ -1,2 +1,3 @@
 -- Don't allow players to replicate containers, since there is a limit to the total number of containers
-item_replicator.bl_add("area_containers:container")
+item_replicator.bl_add("area_containers:container_off")
+
