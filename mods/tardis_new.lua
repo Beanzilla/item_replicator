935c68688fcc6041185dba11a4ae49f478056318
Beanzilla <david2bond2@gmail.com>
2022-07-12 14:39:21 -0400

    Adding tardis_new to mod support
    This prevents the tardis and the vortex manipulator from being replicated...
    
    While also allowing the biscut to be replicated.

---
+++ mods/tardis_new.lua
@@ -0,0 +1,7 @@
+
+-- Allow Biscuts to be replicated insanely fast
+item_replicator.add("tardis_new:biscut", 2, 15)
+
+-- Blacklist things you really don't want replicated
+item_replicator.bl_add("tardis_new:tardis_old")
+item_replicator.bl_add("tardis_new:vortex")
